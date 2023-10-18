1b2440228be4a027b8091c2dd691ed1ad6da7cb8
Alvaro Andrade Ardiles <alvaro.andrade@brics21.cl>
2023-10-17 23:45:08 -0300

    🐛

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Navbar from './components/Navbar';
 
 function App() {
   const {t} = useTranslation()
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {t("welcome")}
-        </p>
+      <Navbar></Navbar>
       </header>
+      <section>
+          {t("welcome")}
+      </section>
     </div>
   );
 }
