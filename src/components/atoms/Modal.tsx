c6a16561435e7d8cdc25bd1d6e4652a36f43c51b
Alvaro Andrade Ardiles <dremian42@gmail.com>
2024-04-14 01:05:10 -0400

    feat 🐛  (Modal):

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Modal.scss'
 import Card from '../molecules/Card'
 
@@ -15,17 +15,30 @@ const Modal = ({
   onClose,
   children,
 }: modalProps) => {
+  const modalRef = useRef<HTMLDivElement>(null)
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      onClose()
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick)
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  })
   const handleClick = () => {
-    onClose()
+    console.log('adentro')
   }
   return (
     <>
       {isOpen ? (
-        <article
-          className="modalContainer"
-          onClick={hadCard ? handleClick : undefined}
-        >
-          <section className="card-modal-section">
+        <article className="modalContainer">
+          <section
+            className="card-modal-section"
+            ref={modalRef}
+            onClick={hadCard ? handleClick : undefined}
+          >
             <Card hasHeader={false} isModal={true}>
               {children}
             </Card>
